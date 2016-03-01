26f300c384d67880217dc421824b00118fbc89ca
isnoth <isnothe@gmail.com>
2016-03-01 15:17:19 +0800

    add re-base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import Rebase from 're-base';
+
+var base = Rebase.createClass('https://jt-ts.firebaseio.com/rebase-chat');
+console.log('Please change to your own firebase address in components/Container.js');
 
 class App extends React.Component {
 
