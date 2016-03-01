258c3a4dc312c26b160f54702332a1f9c1f0081a
isnoth <isnothe@gmail.com>
2016-03-01 09:38:29 +0800

    delete for jspm clean node

--- src/app/main.jsx
+++
@@ -1,8 +0,0 @@
- import React        from 'react'; 
- import ReactDOM     from 'react-dom'; 
- import LayoutComponent   from './components/test.jsx!jsx';      
-    
- ReactDOM.render( 
-   <LayoutComponent/>, 
-   document.getElementById('content') 
- ); 
