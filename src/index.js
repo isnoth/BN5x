2c1c8baba1800438e7002afc38ba003ca6f6379e
isnoth <isnothe@gmail.com>
2016-03-02 12:21:21 +0800

    todo list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,71 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var AddItem = require('./AddItem');
+var List = require('./List');
 import Rebase from 're-base';
 
-var base = Rebase.createClass('https://jt-ts.firebaseio.com/rebase-chat');
-console.log('Please change to your own firebase address in components/Container.js');
+var base = Rebase.createClass('https://test-201601.firebaseio.com/');
 
-class App extends React.Component {
+
+base.push('-KBlVOQst7m8WKxd4cT_', {
+  data: {name: 'George', type: 'Grizzly'},
+  then(){
+    console.log('set ok')
+  }
+})
+
+/*
+base.fetch('-KBMQ3AV_W0sXMT4gB9_', {
+  context: this,
+  asArray: true,
+  then(data){
+    console.log(data);
+  }
+});
+*/
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+  componentDidMount(){
+    this.ref = base.syncState('todoList', {
+      context: this,
+      state: 'list',
+      asArray: true,
+      then(){
+        this.setState({loading: false})
+        console.log(this.state.list)
+      }
+    })
+  }
+  componentWillUnmount(){
+    base.removeBinding(this.ref)
+  }
+  handleAddItem(newItem){
+    this.setState({
+      list: this.state.list.concat([newItem])
+    });
+  }
+  handleRemoveItem(index){
+    var newList = this.state.list;
+    newList.splice(index, 1);
+    this.setState({
+      list: newList
+    })
+  }
 
   render(){
     return (
-      <div style={ { paddingTop: '30px' } }>
-        hello 
+      <div>
+        {this.state.loading === true ? <h3> LOADING... </h3> : <List items={this.state.list} remove={this.handleRemoveItem.bind(this)}/>}
+        <AddItem add={this.handleAddItem.bind(this)} />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
