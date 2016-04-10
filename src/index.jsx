af537d1c911cc19cf2a79c15e98758a0e7f7476e
isnothe@home <isnothe@gmail.com>
2016-04-10 20:11:16 +0800

    remove

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MyButtonController = require('./components/MyButtonController');
-
-ReactDOM.render(<MyButtonController/>, document.getElementById("app")
-);
