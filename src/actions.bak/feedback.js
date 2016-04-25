ecca6a345f3b7eba928994652773629f7f6eaacf
isnothe@home <isnothe@gmail.com>
2016-04-25 20:52:20 +0800

    remove back file

--- src/actions.bak/feedback.js
+++
@@ -1,9 +0,0 @@
-import C from '../constants';
-
-const feedbackActions = { 
-  dismissFeedback(num) {
-		return { type: C.DISMISS_FEEDBACK, num };
-	}
-};
-
-export default feedbackActions;
--- src/actions.bak/index.js
+++
@@ -1,5 +0,0 @@
-import feedbackActions from './feedback';
-import articleActions from './articles';
-import pomodarioActions from './pomodario';
-
-export default Object.assign({}, feedbackActions, articleActions, pomodarioActions);
